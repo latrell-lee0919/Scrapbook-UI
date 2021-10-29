dda3c14e23ecbb93f0e73e6bec633ab1423258e3
Latrell <lll4xy@virginia.edu>
2021-10-28 21:48:34 -0400

    first feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+// component for button to set filter to ''
+
+const Filter = (props) => {
+  const years = props.pictures.map(p => p.year)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="year">Choose a year:</label>
+      <select name="years" id="years" value={props.newFilter} onChange={props.handleFilterChange}>
+        {years.map(year =>
+          <option key={year} value={year}>{year}</option>
+        )}
+      </select>
+    </div>
+  )
+}
+
+const Pictures = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <div> year {props.year}</div>
+      <div> data {props.data}</div>
+    </div>
+  )
+}
+
+const App = () => {
+  //const [pictures, setPictures] = useState([])
+  const [newFilter, setNewFilter] = useState('')
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
+  }
+
+  const pictures = [
+    {
+      year: "2018",
+      data: "testing 2018"
+    },
+    {
+      year: "2017",
+      data: "testing 2017"
+    },
+    {
+      year: "2016",
+      data: "testing 2016"
+    },
+    {
+      year: "2019",
+      data: "testing 2019"
+    },
+    {
+      year: "2020",
+      data: "testing 2020"
+    },
+  ]
+
+  var filtered = pictures.filter(picture => picture.year.includes(newFilter))
+
+  
+
+  return (
+    <div>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} pictures={pictures}/>
+      {filtered.map(picture => 
+        <Pictures 
+        key={picture.year}
+        year={picture.year}
+        data={picture.data}
+        />
+        )}
     </div>
   );
 }
